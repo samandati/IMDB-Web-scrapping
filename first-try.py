d0d3184f5e08c4c8c2a26eec79a5976007d1f5c1
Sam Andati <98655454+samandati@users.noreply.github.com>
2022-08-28 11:38:01 +0300

    update the first-try file

--- first-try.py
+++ first-try.py
@@ -1,2 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+
+
+try: 
+    source = requests.get(" ")
+    source.raise_for_status
+
+    soup = source.Beautifulsoup(text,)
+
+except Exception as e:
+    print(e)
